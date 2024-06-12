ee4747fd93a36a7fd436a91908bbaa3bd8ec7c00
artistXenon <xtramainpp@gmail.com>
2024-06-12 10:52:02 +0900

    update ui

---
+++ initial.sh
@@ -0,0 +1,16 @@
+
+# NEOVIM
+# TODO: logic to select right OS for installation
+#
+# example: https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+#
+curl -LO $neovim_url
+tar -xzvf $neovim_file
+# to something like /usr/local/bin
+#
+# https://github.com/BurntSushi/ripgrep
+# https://github.com/jesseduffield/lazygit
+# 
+
+
+
